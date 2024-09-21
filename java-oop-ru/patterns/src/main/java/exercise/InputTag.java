9e3af6e5f37b8870bdb4d445a01fbb6f6f664672
@hexlet/cli <support@hexlet.io>
2024-09-21 23:49:51 +0300

    submit java-oop-ru/patterns

---
+++ java-oop-ru/patterns/src/main/java/exercise/InputTag.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import lombok.Getter;
+
+// BEGIN
+@Getter
+public class InputTag implements TagInterface{
+    private String type;
+    private String value;
+
+    @Override
+    public String render() {
+        return "<input type=" + getType()  + " value=" + getValue() + ">";
+    }
+
+    public InputTag(String type, String value){
+        this.type = type;
+        this.value = value;
+    }
+}
+// END
---
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import lombok.Getter;
+
+// BEGIN
+@Getter
+public class LabelTag implements TagInterface{
+    private String label;
+    private TagInterface tagInterface;
+
+    @Override
+    public String render() {
+        return "<label>Press Submit" + tagInterface.render() + "</label>";
+    }
+
+    public LabelTag(String label, TagInterface tagInterface){
+        this.label = label;
+        this.tagInterface = tagInterface;
+    }
+}
+// END
